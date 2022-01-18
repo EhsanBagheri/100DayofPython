e073ca95cde4ed67c7f14d47b890f8fbf5083970
Ehsan Bagheri <e2b2000@gmail.com>
2022-01-18 20:16:15 +0330

    Update main.py

--- 1Day/main.py
+++ 1Day/main.py
@@ -2,4 +2,9 @@ w = 1.8
 h = 2.5
 test = True
 
-print(f"w is {w}  h is {h}  test is {test}")
+print(f"w is {w}  h is {h}  test is {test}\n\n\n")
+
+name = input("Enter your name please")
+
+print(name.upper())
+
